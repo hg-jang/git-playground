6edc2b959c24e71f9421fbf2ae612227ac3f5e16
hg-jang <wkdgusrhkd@gmail.com>
2024-01-12 20:48:02 +0900

    feat: 덧셈 기능

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+const $num1Input = document.querySelector("#num1") as HTMLInputElement;
+const $num2Input = document.querySelector("#num2") as HTMLInputElement;
+const $addButton = document.querySelector("#add") as HTMLButtonElement;
+const $addResult = document.querySelector("#add-result") as HTMLSpanElement;
+
+$addButton.addEventListener("click", () => {
+  const num1 = +$num1Input.value;
+  const num2 = +$num2Input.value;
+
+  $addResult.innerText = (num1 + num2).toString();
+});
